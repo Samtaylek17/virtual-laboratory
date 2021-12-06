c7e7549394ba6d38f85cb9d0aa1459430da8b209
Temitayo Ogunsusi <ogunsusitemitayo99@gmail.com>
2021-12-06 22:10:14 +0100

    Basic circuit and measurement

---
+++ Simulations/skidl_lib_sklib.py
@@ -0,0 +1,11 @@
+from skidl import Pin, Part, Alias, SchLib, SKIDL, TEMPLATE
+
+SKIDL_lib_version = '0.0.1'
+
+skidl_lib = SchLib(tool=SKIDL).add_parts(*[
+        Part(**{ 'name':'V', 'dest':TEMPLATE, 'tool':SKIDL, 'description':'Voltage source', 'keywords':'voltage source', '_aliases':Alias({'VS', 'AMMETER', 'v', 'vs', 'ammeter'}), 'pyspice':{'name': 'V', 'kw': {'value': 'dc_value', 'dc_value': 'dc_value', 'p': 'plus', 'n': 'minus'}, 'add': <function add_part_to_circuit at 0x7f8a985338b0>}, 'dc_value':UnitValue(1 V), '_match_pin_regex':False, 'ref_prefix':'V', 'num_units':1, 'fplist':None, 'do_erc':True, 'aliases':Alias({'VS', 'AMMETER', 'v', 'vs', 'ammeter'}), 'pin':None, 'footprint':None, 'pins':[
+            Pin(num='1',name='p',func=Pin.types.PASSIVE,do_erc=True),
+            Pin(num='2',name='n',func=Pin.types.PASSIVE,do_erc=True)] }),
+        Part(**{ 'name':'R', 'dest':TEMPLATE, 'tool':SKIDL, 'description':'Resistor', 'keywords':'res resistor', '_match_pin_regex':False, 'pyspice':{'name': 'R', 'kw': {'value': 'resistance', 'resistance': 'resistance', 'ac': 'ac', 'multiplier': 'multiplier', 'm': 'multiplier', 'scale': 'scale', 'temp': 'temperature', 'temperature': 'temperature', 'dtemp': 'device_temperature', 'device_temperature': 'device_temperature', 'noisy': 'noisy', 'p': 'plus', 'n': 'minus'}, 'add': <function add_part_to_circuit at 0x7f8a985338b0>}, 'ref_prefix':'R', 'num_units':1, 'fplist':None, 'do_erc':True, 'aliases':Alias(), 'pin':None, 'footprint':None, 'pins':[
+            Pin(num='1',name='p',func=Pin.types.PASSIVE,do_erc=True),
+            Pin(num='2',name='n',func=Pin.types.PASSIVE,do_erc=True)] })])
